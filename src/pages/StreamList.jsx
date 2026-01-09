d79a10a111b8bec0909fef979abae4c4d3397185
ExgleGaming <EaglesFan91x@outlook.com>
2026-01-08 21:23:40 -0500

    added functionality to stream list page

--- src/pages/StreamList.jsx
+++ src/pages/StreamList.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './StreamList.css';
 
 function StreamList() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('User Input: ', inputValue);
+        setInputValue('');
+    }
+
     return (
         <div className='streamlist'>
             <h1>Stream List</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={inputValue}
+                    onChange={(event) => setInputValue(event.target.value)}
+                    placeholder='Enter a movie or show...'
+                />
+                <button type='submit'>Add</button>
+            </form>
         </div>
     );
-};
+}
 
 export default StreamList;
